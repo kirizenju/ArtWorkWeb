99c79007b074621da4fd5605ccc4d4988a48e581
GiangNguyen <133587884+NooPhuocGiang@users.noreply.github.com>
2024-03-03 14:14:49 +0700

    api get all product user/ api get imageurl

---
+++ DataTier/Repository/Implement/ProductRepository.cs
@@ -0,0 +1,57 @@
+﻿using DataTier.Models;
+using DataTier.Repository.Interface;
+using DataTier.View.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTier.Repository.Implement
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly projectSWDContext _context;
+
+        public ProductRepository(projectSWDContext context)
+        {
+            _context = context;
+        }
+
+        public List<UserProductViewModel> GetAllProductUser(int id)
+        {
+            var artworks = _context.Artworks.Where(e => e.Owner == id).ToList();
+            var users = _context.Users.ToDictionary(u => u.IdUser);
+
+            var userProductViewModels = artworks.Select(artwork =>
+            {
+                if (!users.TryGetValue(artwork.Owner ?? 0, out var user))
+                    return null;
+
+                return new UserProductViewModel
+                {
+                    IdArtwork = artwork.IdArtwork,
+                    Name = artwork.Name,
+                    IdUser = artwork.Owner ?? 0,
+                    Username = user.Username,
+                    CategoryName = artwork.CategoryName,
+                    Status = artwork.Status,
+                    Price = artwork.Price,
+                    Address = user.Address,
+                };
+            }).Where(viewModel => viewModel != null).ToList();
+
+            return userProductViewModels;
+        }
+
+        public List<ImageViewModel> GetImageURL(int artworkid)
+        {
+            var imageUrls = _context.ImageLists
+                .Where(image => image.ArtworkId == artworkid)
+                .Select(image => new ImageViewModel { ImageUrl = image.ImageUrl })
+                .ToList();
+
+            return imageUrls;
+        }
+    }
+}
---
+++ DataTier/View/Common/MessageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTier.View.Common
+{
+    public class MessageViewModel
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
