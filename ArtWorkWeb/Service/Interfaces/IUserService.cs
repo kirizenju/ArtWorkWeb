e3f5a6dd62f42692e097888b5003ca5453d70914
PhamMinhThiense160345 <135082170+PhamMinhThiense160345@users.noreply.github.com>
2024-03-20 00:30:23 +0700

    admin

--- ArtWorkWeb/Service/Interfaces/IUserService.cs
+++ ArtWorkWeb/Service/Interfaces/IUserService.cs
@@ -11,5 +11,6 @@ namespace ArtWorkWeb.Service.Interfaces
         Task<LoginResponse> Login(LoginRequest loginRequest);
 
         MessageViewModel UpdateProfile(ProfileUpdateRequest model);
+
     }
 }
