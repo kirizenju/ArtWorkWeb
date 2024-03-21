a3411f8f6d376c891287ca62fe6ae16ef78a7bb0
PhamMinhThiense160345 <135082170+PhamMinhThiense160345@users.noreply.github.com>
2024-03-21 11:00:57 +0700

    ad

--- ArtWorkWeb/Controllers/AdminController.cs
+++ ArtWorkWeb/Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using DataTier.Models;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace YourProjectNamespace.Controllers
 {
@@ -10,13 +11,15 @@ namespace YourProjectNamespace.Controllers
     public class AdminController : ControllerBase
     {
         private readonly projectSWDContext _context;
+        private readonly AdminControllerHelper _helper;
 
-        public AdminController(projectSWDContext context)
+        public AdminController(projectSWDContext context, AdminControllerHelper helper)
         {
             _context = context;
+            _helper = helper;
         }
 
-        
+        // PUT: api/Admin/BanUser/{id}
         [HttpPut("BanUser/{id}")]
         public async Task<IActionResult> BanUser(int id)
         {
@@ -27,8 +30,7 @@ namespace YourProjectNamespace.Controllers
                 return NotFound();
             }
 
-            
-            //user.IsActive = false; // 
+            user.IsActive = false; // Ban the user by setting IsActive to false
 
             _context.Entry(user).State = EntityState.Modified;
 
@@ -38,7 +40,7 @@ namespace YourProjectNamespace.Controllers
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!UserExists(id))
+                if (!_helper.UserExists(id))
                 {
                     return NotFound();
                 }
@@ -51,9 +53,53 @@ namespace YourProjectNamespace.Controllers
             return NoContent();
         }
 
-        private bool UserExists(int id)
+        // PUT: api/Admin/UnbanUser/{id}
+        [HttpPut("UnbanUser/{id}")]
+        public async Task<IActionResult> UnbanUser(int id)
         {
-            return _context.Users.Any(e => e.IdUser == id);
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.IsActive = true; // Unban the user by setting IsActive to true
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_helper.UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
+    }
+}
+
+public class AdminControllerHelper
+{
+    private readonly projectSWDContext _context;
+
+    public AdminControllerHelper(projectSWDContext context)
+    {
+        _context = context;
+    }
+
+    public bool UserExists(int id)
+    {
+        return _context.Users.Any(e => e.IdUser == id);
     }
 }
