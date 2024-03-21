4e7bf46716407b0f4ae6b5d66137a08e6fecb6b4
GiangNguyen <133587884+NooPhuocGiang@users.noreply.github.com>
2024-03-21 09:29:20 +0700

    fix get all artwork

--- ArtWorkWeb/Service/Implement/ArWorkService.cs
+++ ArtWorkWeb/Service/Implement/ArWorkService.cs
@@ -41,8 +41,20 @@ namespace ArtWorkWeb.Service.Implement
 
         public async Task<IPaginate<GetArtWorkResponse>> GetAllArtWorks(ArtWorkFilter filter, PagingModel pagingModel)
         {
+            // Example of authorizing user filter.Owner = 1;
             IPaginate<GetArtWorkResponse> response = await _unitOfWork.GetRepository<Artwork>().GetPagingListAsync(
-                selector: x => _mapper.Map<GetArtWorkResponse>(x),
+                //selector: x => _mapper.Map<GetArtWorkResponse>(x),
+                selector: a => new GetArtWorkResponse
+                {
+                    IdArtwork = a.IdArtwork,
+                    Name = a.Name,
+                    Price = a.Price,
+                    Owner = a.Owner,
+                    Status = a.Status,
+                    CategoryName = a.CategoryName,
+                    Author = a.Author,
+                    ImageLists = a.ImageLists
+                },
                 filter: filter,
                 orderBy: x => x.OrderBy(x => x.Name),
                 page: pagingModel.page,
